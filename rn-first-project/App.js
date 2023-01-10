fab951361786a5984416bebbb1b519de132d7cce
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-01-10 15:14:49 +0200

    add input form

--- rn-first-project/App.js
+++ rn-first-project/App.js
@@ -1,26 +1,85 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+  Text,
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const nameHandler = (text) => setName(text);
+  const passwordHandler = (text) => setPassword(text);
+  const emailHandler = (text) => setEmail(text);
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>YURA HELLO!</Text>
-      <Text>SUPER!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            value={name}
+            onChangeText={nameHandler}
+            placeholder="Логин"
+            style={styles.input}
+          />
+          <TextInput
+            value={email}
+            onChangeText={emailHandler}
+            placeholder="Почта"
+            style={styles.input}
+          />
+          <TextInput
+            value={password}
+            onChangeText={passwordHandler}
+            placeholder="Пароль"
+            secureTextEntry={true}
+            style={styles.input}
+          />
+          <Button
+            title={"Зарегистрироваться"}
+            style={styles.buttonReg}
+            onPress={onLogin}
+          />
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#ecf0f1",
   },
-  text: {
-    color: "red",
-
-    fontWeight: "900",
+  input: {
+    width: 343,
+    height: 50,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  buttonReg: {
+    width: 343,
+    borderRadius: 8,
+    borderColor: "red",
   },
 });
